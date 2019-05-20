9731ad2cd52ef9971672e802815c90915bb371a2
mikaBunting <mika.bunting@gmail.com>
2019-05-20 15:11:54 +0200

    added basic sync of player data which includes items, objectives, role. Improved turn indication when its players turn

--- client/src/index.ts
+++ client/src/index.ts
@@ -64,7 +64,6 @@ io.on('joinedLobby', () => {
 
 // game loop info refresh
 io.on('gameStatus', (data: any) => {
-	console.log('game loop update')
 	// displays lobby name
 	document.getElementById('lobbyName').innerText = data.lobby.name;
 	
@@ -78,20 +77,62 @@ io.on('gameStatus', (data: any) => {
 	var html = "";
 	data.lobby.players.map((p: any) => { html += `<span class="player">${p.username} <span class="role">${p.role}</span></span>`});
 	document.getElementById('playersBox').innerHTML = html;
+
+	// removes turn class from app
+
+	$('#app').removeClass('onTurn');
+
+	// asks for player data
+	io.emit('requestPlayerData');
 });
 
+
+// updated and displays player data
+io.on('updatePlayerData', (data: any) => {
+
+	//displays objective
+	$('#objective').html(data.objective);
+
+	// displays role info
+	$('#role').html(data.role);
+
+	// changes turn to yours
+	if(data.isOnTurn) {
+		$('#turnName').html('Yours turn!');
+		$('#app').addClass('onTurn');
+	} else {
+		$('#app').removeClass('onTurn');
+	}
+	
+	// update player inventory
+	$('#inventory').html('');
+
+	data.items.map((i: any) => {
+		$('#inventory').append(`<div title="${i.info}" class="${i.cssClass}">${i.name}</div>`);
+	});
+	
+})
+
+
+// displays chat message
 io.on('chatMessage', (data: any) => {
 	
 	if(data !== undefined) {
 		
 		let chatBox = $('#chatContainer');
 		let html = `<span class="chat--item ${data.isMine ? 'right' : ''}">
-			<span class="chat--name">${data.from}</span>
+			<span class="chat--name">${data.isMine? "Me" : data.from}</span>
 			<span class="chat--message">${data.message}</span>
 			</span>`;
 		chatBox.append(html);	
 	}
 });
+
+
+io.on('updateTurn', () => {
+	console.log('I am on turn!');
+})
+
 
 // ends player turn 
 let endTurn = () => {
@@ -100,10 +141,8 @@ let endTurn = () => {
 
 // send chat message to server
 let sendChatMessage = () => {
-	
 	let value = $('#chatInput').val();
 	$('#chatInput').val('');
-	alert(value);
 	io.emit('sendChatMessage', value);
 }
 
